651dac4ae5b857fd2e3d41f923faa3b1a3567a05
xiusin <826466266@qq.com>
2022-03-11 19:07:49 +0800

    优化登录页面

--- src/cool/modules/base/views/tags.vue
+++ src/cool/modules/base/views/tags.vue
@@ -1,105 +1,17 @@
-<style lang="scss">
-.monaco-with-tree {
-	height: 100%;
-	background: #3a3a3a;
-	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
-		"Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-	-webkit-font-smoothing: antialiased;
-	&.full-screen {
-		position: fixed;
-		left: 0;
-		top: 0;
-		width: 100vw;
-		height: 100vh !important;
-		z-index: 100000;
-	}
-	.monaco-menu-pane {
-		height: 100%;
-	}
-	.monaco-right-pane {
-		height: 100%;
-		position: relative;
-		.vue-tabs-chrome {
-			*,
-			*::before,
-			*::after {
-				-webkit-box-sizing: border-box;
-				box-sizing: border-box;
-			}
-			font-size: 14px;
-			.tabs-item.monaco-icon-label::before {
-				box-sizing: content-box;
-				position: absolute;
-				left: 22px;
-				top: 5px;
-				z-index: 1;
-			}
-		}
-		.no-tab-pane {
-			background-color: #1e1e1e;
-			text-align: center;
-			position: absolute;
-			left: 0;
-			top: 48px;
-			width: 100%;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-		}
-	}
-	.btn-fullscreen {
-		position: absolute;
-		top: 10px;
-		right: 8px;
-		z-index: 2000;
-		color: white;
-		width: 24px;
-		height: 24px;
-		text-align: center;
-		line-height: 24px;
-		cursor: pointer;
-		opacity: 0.8;
-		&.fullscreen {
-			position: fixed;
-		}
-		&:hover {
-			opacity: 1;
-		}
-	}
-}
-</style>
 <template>
 	<div ref="wrapper" :class="`monaco-with-tree${isFullScreen ? ' full-screen' : ''}`">
-		<splitpanes style="height: 400px">
-			<pane min-size="20">
+		<splitpanes style="height: 100%">
+			<pane size="20">
 				<div ref="menu" class="monaco-menu-pane"></div>
 			</pane>
 			<pane>
 				<div class="monaco-right-pane">
-					<!-- insert-to-after：添加tab时插入到当前tab后面 -->
-<!--					<vue-tabs-chrome-->
-<!--						ref="tab"-->
-<!--						theme="dark"-->
-<!--						v-model="currentTab"-->
-<!--						:tabs="tabs"-->
-<!--						insert-to-after-->
-<!--					/>-->
-					<i
-						@click="toggleFullScreen"
-						:class="[
-							'btn-fullscreen',
-							'luyou-icon',
-							isFullScreen ? 'icontuichuquanping' : 'iconquanping1',
-							isFullScreen ? 'fullscreen' : ''
-						]"
-					></i>
+					<i @click="toggleFullScreen"
+					   :class=" 'btn-fullscreen ' + ( isFullScreen ? 'el-icon-s-grid' : 'el-icon-menu') "></i>
 					<!-- monaco初始化的时候高度必须确定好，所以这里用visibility来隐藏 -->
 					<div
 						ref="monaco"
-						:style="{
-							height: `calc(100% - ${tabHeight}px)`,
-							visibility2: currentTab ? 'visible' : 'hidden'
-						}"
+						:style="{	height: `calc(100% - ${tabHeight}px)`,	visibility2: currentTab ? 'visible' : 'hidden'	}"
 					></div>
 					<div
 						v-show="!currentTab"
@@ -111,24 +23,25 @@
 				</div>
 			</pane>
 		</splitpanes>
+		<div>
+			<div class="row"> <div class="path woo-copy">/www/wwwroot/demo2.wooadmin.cn/public/h5/static/iconfont/iconfont.css</div> <div class="status"> <div class="cursor item">行:1,列:14</div> <div class="charset item">编码:UTF-8</div> <div class="tab item" data-type="css">Tab空格:2</div>  <div class="lang item">语言:css</div>  <div class="readonly item"> <i class="ace-icon ace-icon-24gl-unlock2"></i> </div> </div> </div>
+		</div>
 	</div>
 </template>
 <script>
-import VueTabsChrome from "vue3-tabs-chrome";
+import Vue3TabsChrome from "vue3-tabs-chrome";
 import NiceMonacoTree from "nice-monaco-tree";
 import * as monaco from "monaco-editor";
 
-// In your Vue component.
-import { Splitpanes, Pane } from "splitpanes";
+import {Pane, Splitpanes} from "splitpanes";
 import "splitpanes/dist/splitpanes.css";
 
-let monacoDiffEditor = null;
 let monacoEditor = null;
 let monacoTree = null;
 
 export default {
 	components: {
-		VueTabsChrome,
+		Vue3TabsChrome,
 		Splitpanes,
 		Pane
 	},
@@ -148,18 +61,16 @@ export default {
 		// 暂时只支持传一个文件
 		defaultOpenFiles: {
 			type: Array,
-			// default: () => [],
-			default: () => ["README.md"]
+			default: () => [],
 		},
 		readonly: {
 			type: Boolean,
-			default: true
+			default: false
 		},
 		getFileContent: {
 			type: Function,
 			default: (filePath) => {
-				// return `${filePath}-left`
-				return [`${filePath}-left`, `${filePath}-right`];
+				return [`${filePath}`]
 			}
 		},
 		getFileTitle: {
@@ -196,7 +107,8 @@ export default {
 			}
 		}
 	},
-	created() {},
+	created() {
+	},
 	mounted() {
 		// 由于split组件必须按百分比设置宽度，这里手动计算
 		const totalWith =
@@ -211,9 +123,7 @@ export default {
 	// 组件销毁事件
 	unmounted() {
 		// 销毁编辑器
-		monacoDiffEditor && monacoDiffEditor.dispose();
 		monacoEditor && monacoEditor.dispose();
-		monacoDiffEditor = null;
 		monacoEditor = null;
 	},
 	methods: {
@@ -238,9 +148,7 @@ export default {
 		getMonacoTree() {
 			return monacoTree;
 		},
-		getMonacoDiffEditor() {
-			return monacoDiffEditor;
-		},
+
 		// 选中并打开某个文件
 		setSelection(filePath) {
 			monacoTree && monacoTree.setSelection(filePath);
@@ -256,34 +164,17 @@ export default {
 			};
 			const ext = filePath.slice(filePath.lastIndexOf(".") + 1);
 			const language = modeMap[ext.toLowerCase()] || "javascript";
-			if (!monacoDiffEditor) {
-				// 如果不是diff模式
-				if (!right) {
-					monacoEditor = monaco.editor.create(this.$refs.monaco, {
-						theme: "vs-dark",
-						fontSize: "13px",
-						readOnly: true,
-						// todo 切换文件时需要修改语言
-						language
-					});
-				} else {
-					monacoDiffEditor = monaco.editor.createDiffEditor(this.$refs.monaco, {
-						// 禁用分割线resize
-						enableSplitViewResizing: false,
-						theme: "vs-dark",
-						fontSize: "13px",
-						readOnly: true,
-						automaticLayout: true
-					});
-				}
+			if (!monacoEditor) {
+				monacoEditor = monaco.editor.create(this.$refs.monaco, {
+					theme: "vs-dark",
+					fontSize: "12px",
+					readOnly: false,
+					// todo 切换文件时需要修改语言
+					language
+				});
 			}
-			if (!right) {
-				monacoEditor.setValue(left);
-			} else {
-				const original = monaco.editor.createModel(left, language);
-				const modified = monaco.editor.createModel(right, language);
-				monacoDiffEditor.setModel({ original, modified });
-			}
+
+			monacoEditor.setValue(left);
 		},
 		openFile(filePath, file, fileIcon, isDoubleClick = true) {
 			// const idx = this.tabs.findIndex(item => item.key === filePath);
@@ -294,25 +185,117 @@ export default {
 				}
 			} else {
 				if (isDoubleClick) {
-					this.$refs.tab.addTab({
-						label: this.getFileTitle(filePath),
-						key: filePath,
-						closable: true,
-						// 默认的icon只支持传图片，这里我们直接使用monaco的icon class来实现图标展示
-						// 通过设置一个空的favicon来设置一个占位符
-						favicon: (h) => h("span"),
-						class: `monaco-icon-label ${fileIcon}`
-					});
+					// this.$refs.tab.addTab({
+					// 	label: this.getFileTitle(filePath),
+					// 	key: filePath,
+					// 	closable: true,
+					// 	// 默认的icon只支持传图片，这里我们直接使用monaco的icon class来实现图标展示
+					// 	// 通过设置一个空的favicon来设置一个占位符
+					// 	favicon: (h) => h("span"),
+					// 	class: `monaco-icon-label ${fileIcon}`
+					// });
 					this.currentTab = filePath;
 				} else {
 				}
 			}
 			this.initMonacoEditor(filePath);
 		},
-		resize() {},
+		resize() {
+		},
 		toggleFullScreen() {
 			this.isFullScreen = !this.isFullScreen;
 		}
 	}
 };
 </script>
+
+
+<style lang="scss">
+.monaco-with-tree {
+	height: 100%;
+	background: #3a3a3a;
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
+	"Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+	-webkit-font-smoothing: antialiased;
+
+&
+.full-screen {
+	position: fixed;
+	left: 0;
+	top: 0;
+	width: 100vw;
+	height: 100vh !important;
+	z-index: 100000;
+}
+
+.monaco-menu-pane {
+	height: 100%;
+}
+
+.monaco-right-pane {
+	height: 100%;
+	position: relative;
+
+.vue-tabs-chrome {
+
+*,
+*::before,
+*::after {
+	-webkit-box-sizing: border-box;
+	box-sizing: border-box;
+}
+
+font-size:
+
+14
+px
+
+;
+.tabs-item.monaco-icon-label::before {
+	box-sizing: content-box;
+	position: absolute;
+	left: 22px;
+	top: 5px;
+	z-index: 1;
+}
+
+}
+.no-tab-pane {
+	background-color: #1e1e1e;
+	text-align: center;
+	position: absolute;
+	left: 0;
+	top: 48px;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+}
+.btn-fullscreen {
+	position: absolute;
+	top: 10px;
+	right: 8px;
+	z-index: 2000;
+	color: white;
+	width: 24px;
+	height: 24px;
+	text-align: center;
+	line-height: 24px;
+	cursor: pointer;
+	opacity: 0.8;
+
+&
+.fullscreen {
+	position: fixed;
+}
+
+&
+:hover {
+	opacity: 1;
+}
+
+}
+}
+</style>
